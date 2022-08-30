04a454d6d01b61b8c3ac35e52407eee2ffca7dfc
codinci <dkibau@gmail.com>
2022-08-30 10:39:21 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -3,8 +3,42 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeButtons = document.getElementsByClassName('like-glyph');
 
+document.addEventListener('DOMContentLoaded', () => {
+  // console.log(likeButtons);
+  Array.from(likeButtons).forEach(likeButton => {
+    likeButton.addEventListener('click', () => {
+      mimicServerCall()
+      .then((result) => {
+        // console.log(result);
+        if(likeButton.textContent === EMPTY_HEART) {
+          // console.log('Change heart');
+          likeButton.textContent = FULL_HEART;
+          likeButton.classList.add('activated-heart');
+        } else {
+          likeButton.textContent = EMPTY_HEART;
+          likeButton.classList.remove('activated-heart');
+        }
+      }).catch((err) => {
+        handleError(err);
+      });
+    })
+  });
+})
 
+function handleError(errorMessage) {
+  // console.log(errorMessage);
+  const errorModal = document.getElementById('modal');
+  errorModal.classList.remove('hidden');
+  const errorText = document.getElementById('modal-message');
+  errorText.innerText = errorMessage;
+  if(!errorModal.classList.contains('hidden')) {
+    setTimeout(() => {
+      errorModal.classList.add('hidden')
+    }, 3000);
+  }
+}
 
 
 //------------------------------------------------------------------------------
